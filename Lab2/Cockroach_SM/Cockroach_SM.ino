0229072c7793b1d9b20bf1a8dd0738bfc6d4a9fa
Tom Owlett <tomowlett@gmail.com>
2015-01-25 20:34:29 -0800

    Cleaned up the code. Works

--- Lab2/Cockroach_SM/Cockroach_SM.ino
+++ Lab2/Cockroach_SM/Cockroach_SM.ino
@@ -16,7 +16,7 @@
 #define STOP               3
 
 /*----------------Module Variables --------------------------*/
-static unsigned char state; // Global variable for the state 
+static unsigned char state = FORWARD; // Global variable for the state 
 
 /*---------------- Module Function Prototypes ---------------*/
 unsigned char TestForLightOn(void);
@@ -26,76 +26,42 @@ void RespToLightOff(void);
 unsigned char TestForBump(void);
 void RespToBump(void);
 unsigned char TestBackupExpired(void);
-void RespBackupExpired(void);
 
 /*---------------- Arduino Main Functions -------------------*/
 void setup() {  // setup() function required for Arduino
   Serial.begin(9600);
   Serial.println("Starting Program...");
   RoachInit();
-  TMRArd_InitTimer(0, TIME_INTERVAL);
 }
 
-void loop() {
-  if (TestForLightOn()) RespToLightOn();
-  if (TestForLightOff()) RespToLightOff();
-  if (TestForBump()) RespToBump();
-  if (TestBackupExpired()) RespBackupExpired();
-  //if (TMRArd_IsTimerExpired(2) == TMRArd_EXPIRED) RespBackupExpired();
-  
+void loop() {  
   switch(state) {
     case(FORWARD):
-      LeftMtrSpeed(10);
-      RightMtrSpeed(10);
+      if (TestForBump()) {
+        RespToBump();
+        state = BACKWARD;
+      }
+      if (TestForLightOff()) {
+        RespToLightOff();
+        state = STOP;
+      }
       break;
     case(STOP):
-      LeftMtrSpeed(0);
-      RightMtrSpeed(0);
+      if (TestForLightOn()) {
+        RespToLightOn();
+        state = FORWARD;
+      }
       break;
     case(BACKWARD):
-        unsigned char bumper;
-        bumper = GET_SHARED_BYTE();
-        state = BACKWARD;
-        // display which bumper(s) were hit
-        switch (bumper) {
-            case (0xD0):  Serial.println("  Front Right..."); 
-              LeftMtrSpeed(10);
-              RightMtrSpeed(0);
-              break;
-            case (0xE0):  Serial.println("  Front Left...");
-              LeftMtrSpeed(0);
-              RightMtrSpeed(10);
-              break;
-            case (0x70):  Serial.println("  Back Left..."); 
-              LeftMtrSpeed(-10);
-              RightMtrSpeed(0);
-              break;
-            case (0xB0):  Serial.println("  Back Right..."); 
-              LeftMtrSpeed(0);
-              RightMtrSpeed(-10);
-              break;
-    
-            case (0xC0):  Serial.println("  Both Front ...");
-              LeftMtrSpeed(10);
-              RightMtrSpeed(0);
-              break;
-            case (0x30):  Serial.println("  Both Back...");
-              LeftMtrSpeed(-10);
-              RightMtrSpeed(0);
-              break;
-            case (0x90):  Serial.println("  Both Right...");
-              LeftMtrSpeed(-10);
-              RightMtrSpeed(0);
-              break;
-            case (0x60):  Serial.println("  Both Left...");
-              LeftMtrSpeed(0);
-              RightMtrSpeed(10);
-              break;
-    
-            // should never get here unless borked
-            default:      Serial.print("  What is this I don�t even->");
-                          Serial.println(bumper,HEX);
+      if (TestBackupExpired()) {
+        if (TestForLightOn()) {
+          RespToLightOn();
+          state = FORWARD;
+        } else {
+          RespToLightOff();
+          state = STOP;
         }
+      }
       break;  
   }
 }
@@ -106,45 +72,37 @@ unsigned char TestBackupExpired(void) {
 
 unsigned char TestForLightOn(void) {
   char EventOccurred;
-  unsigned int ThisLight;
-  static unsigned int LastLight = 0;
-  
+  unsigned int ThisLight;  
   ThisLight = LightLevel();
-  EventOccurred = ((ThisLight >= HI_LIGHT_THRESHOLD)
-                       && (LastLight < HI_LIGHT_THRESHOLD));
-
-  LastLight = ThisLight;
+  EventOccurred = (ThisLight >= HI_LIGHT_THRESHOLD);
   return EventOccurred;
 }
 
 void RespToLightOn(void) {
   Serial.println("  ON");
+   LeftMtrSpeed(10);
+   RightMtrSpeed(10);
   state = FORWARD;
 }
 
 unsigned char TestForLightOff(void) {
   char EventOccurred;
   unsigned int ThisLight;
-  static unsigned int LastLight = 0;
-
   ThisLight = LightLevel();
-  EventOccurred = ((ThisLight < LO_LIGHT_THRESHOLD)
-                       && (LastLight >= LO_LIGHT_THRESHOLD));
-
-  LastLight = ThisLight;
+  EventOccurred = (ThisLight < LO_LIGHT_THRESHOLD);
   return EventOccurred;
 }
 
 void RespToLightOff(void) {
   Serial.println("  OFF");
-  state = STOP;
+  LeftMtrSpeed(0);
+  RightMtrSpeed(0);
 }
 
 unsigned char TestForBump(void) {
   static unsigned char LastBumper = 0xF0;
   unsigned char bumper;
   unsigned char EventOccurred;
-  
   bumper = ReadBumpers();
   EventOccurred = ((bumper != 0xF0) && (bumper != LastBumper));
   if (EventOccurred) {
@@ -156,17 +114,46 @@ unsigned char TestForBump(void) {
 
 void RespToBump(void) {
   TMRArd_InitTimer(2, BACKUPTIME);
-  state = BACKWARD;
-}
+  unsigned char bumper;
+  bumper = GET_SHARED_BYTE();
+  
+  switch (bumper) {
+    case (0xD0):  Serial.println("  Front Right..."); 
+      LeftMtrSpeed(10);
+      RightMtrSpeed(0);
+      break;
+    case (0xE0):  Serial.println("  Front Left...");
+      LeftMtrSpeed(0);
+      RightMtrSpeed(10);
+      break;
+    case (0x70):  Serial.println("  Back Left..."); 
+      LeftMtrSpeed(-10);
+      RightMtrSpeed(0);
+      break;
+    case (0xB0):  Serial.println("  Back Right..."); 
+      LeftMtrSpeed(0);
+      RightMtrSpeed(-10);
+      break;
 
-void RespBackupExpired(void) {
-  Serial.println("Backup Timer Expired");
-  LeftMtrSpeed(10);
-  RightMtrSpeed(10);
-  TMRArd_ClearTimerExpired(2);
-  if (LightLevel() >= LO_LIGHT_THRESHOLD) {
-    state = FORWARD;
-  } else {
-    state = STOP;
+    case (0xC0):  Serial.println("  Both Front ...");
+      LeftMtrSpeed(10);
+      RightMtrSpeed(0);
+      break;
+    case (0x30):  Serial.println("  Both Back...");
+      LeftMtrSpeed(-10);
+      RightMtrSpeed(0);
+      break;
+    case (0x90):  Serial.println("  Both Right...");
+      LeftMtrSpeed(-10);
+      RightMtrSpeed(0);
+      break;
+    case (0x60):  Serial.println("  Both Left...");
+      LeftMtrSpeed(0);
+      RightMtrSpeed(10);
+      break;
+
+    // should never get here unless borked
+    default:      Serial.print("  What is this I don�t even->");
+                  Serial.println(bumper,HEX);
   }
 }
